7ee68adc851b096a377f599fe2a52144591140eb
yiyang1016 <yiyang908@gmail.com>
2020-09-05 21:43:45 +0800

    temperature

--- app/src/main/java/com/example/smartfarm/plantmonitor.kt
+++ app/src/main/java/com/example/smartfarm/plantmonitor.kt
@@ -1,25 +1,18 @@
 package com.example.smartfarm
 
-import android.content.Intent
 import android.graphics.Color
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import android.widget.ImageView
 import android.widget.Toast
 import androidx.annotation.RequiresApi
-import androidx.appcompat.app.ActionBar
-import androidx.core.app.NotificationCompat
-import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ktx.database
-import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
-import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import kotlinx.android.synthetic.main.activity_plantmonitor.*
 import java.text.SimpleDateFormat
 //import com.google.firebase.quickstart.database.databinding.ActivityNewPostBinding
@@ -31,7 +24,10 @@ import java.util.*
 import kotlin.concurrent.fixedRateTimer
 import kotlin.concurrent.scheduleAtFixedRate
 //import com.google.firebase.referencecode.storage.R
-import com.google.firebase.storage.ktx.storage
+//import com.google.firebase.storage.ktx.storage
+import android.view.Window
+import android.view.WindowManager
+import androidx.core.content.ContextCompat
 
 class plantmonitor : AppCompatActivity() {
     //private lateinit var mUser : User
@@ -47,6 +43,12 @@ class plantmonitor : AppCompatActivity() {
 
         val actionBar = supportActionBar
         actionBar!!.title = "Farm Security"
+
+        val window: Window = this@plantmonitor.window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.statusBarColor = ContextCompat.getColor(this@plantmonitor, R.color.actionBarColor)
+
         //local variable declaration
         var detectdistance : String = ""
         var detectSound : String = ""
@@ -60,9 +62,25 @@ class plantmonitor : AppCompatActivity() {
             if(convertDistance < 30.0 || convertSound > 508){
                 msstxt.text = "Intruder!!!"//508 = 60db(Sound)
                 msstxt.setTextColor(Color.parseColor("#FF0000"))
+                msstxt.setTextSize(60F)
+                //close buzzer and led
+                var map1 = mutableMapOf<String,Any>()
+                map1["buzzer"] = "1"
+                var map2 = mutableMapOf<String,Any>()
+                map2["led"] = "1"
+
+                mDatabase.child("PI_06_CONTROL")
+                    .updateChildren(map1)
+                mDatabase.child("PI_06_CONTROL")
+                    .updateChildren(map2)
+                buzzerStatustxt.text = "Buzzer is On"
+
+                val text = "Alarm Activated"
+                Toast.makeText(applicationContext, text, Toast.LENGTH_SHORT).show()
             }else{
                 msstxt.text = "Still Safe"
                 msstxt.setTextColor(Color.parseColor("#7CFC00"))
+                msstxt.setTextSize(60F)
             }
         }
 
@@ -99,53 +117,35 @@ class plantmonitor : AppCompatActivity() {
                             detectdistance = p0.child("ultra2").value.toString()
                             detectSound = p0.child("sound").value.toString()
 
-                            verifyDistanceandSound()
-                            distance.setText("Sound = "+p0.child("sound").value.toString())
-                            Ultra2.text = "Ultra 2 = " + p0.child("ultra2").value.toString()
-                            ultra.text = "Date = " + dateText+hourText+minSecText
+                            //distance.setText("Sound = "+p0.child("sound").value.toString()+" dB")
+                            //Ultra2.text = "Ultra 2 = " + p0.child("ultra2").value.toString()
+                            //ultra.text = "Date = " + dateText+hourText+minSecText
                         }
                     }
                 }
             })
         }
-        //get intruder image from firebase
-        fun getIntruderImage(){
-            // Reference to an image file in Cloud Storage
-            val storageReference = Firebase.storage.reference.child("PI_06_CONTROL/")
-
-            // ImageView in your Activity
-            val imageView = findViewById<ImageView>(R.id.intruderPhoto)
-
-            // Download directly from StorageReference using Glide
-            // (See MyAppGlideModule for Loader registration)
-            Glide.with(this /* context */)
-                .load(storageReference)
-                .into(imageView)
-        }
 
         //Button Function
         //Security Status Switch
-        getIntruderImage()
-        val timer = Timer("schedule", true)
+        var timer = Timer("schedule", true)
         var counter: Int = 0
+
+        timer.scheduleAtFixedRate(1000,1000) {
+            readData()
+        }
         swtAlertMode.setOnCheckedChangeListener{
                 buttonView, isChecked ->
             if(isChecked) {
                 //Start read data from firebase
-                if(counter == 0){
-                    timer.scheduleAtFixedRate(1000,1000){
-                        readData()
-                    }
-                }else{
-                    super.onResume()
+                timer.scheduleAtFixedRate(1000,1000){
+                    readData()
+                    verifyDistanceandSound()
                 }
             }else{
-                //Stop retrieving data from firebase
-                //timer.cancel()
-                super.onPause()
-                counter == 1
-
-                //mDatabase.removeEventListener(statelistener)
+                timer.scheduleAtFixedRate(1000,1000) {
+                    readData()
+                }
                 val text = "Alarm Closed Successfully"
                 Toast.makeText(applicationContext, text, Toast.LENGTH_SHORT).show()
             }
@@ -174,10 +174,10 @@ class plantmonitor : AppCompatActivity() {
             })
             //verify buzzer status
             if(buzzerStatus.equals("0")){
-
                 val text = "Alarm not activate"
                 Toast.makeText(applicationContext, text, Toast.LENGTH_SHORT).show()
             }else{
+                //close buzzer and led
                 var map1 = mutableMapOf<String,Any>()
                 map1["buzzer"] = "0"
                 var map2 = mutableMapOf<String,Any>()

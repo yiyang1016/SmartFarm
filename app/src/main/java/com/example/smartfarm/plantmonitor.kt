96526c097ec573e7f9b06a8bedaa657dfa9a7b9f
yiyang1016 <yiyang908@gmail.com>
2020-09-05 14:05:32 +0800

    Able to read firebase data every ten second

--- app/src/main/java/com/example/smartfarm/plantmonitor.kt
+++ app/src/main/java/com/example/smartfarm/plantmonitor.kt
@@ -21,6 +21,8 @@ import java.text.SimpleDateFormat
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.util.*
+import kotlin.concurrent.fixedRateTimer
+import kotlin.concurrent.scheduleAtFixedRate
 
 class plantmonitor : AppCompatActivity() {
     //private val TAG ="ProfileActivity"
@@ -39,63 +41,75 @@ class plantmonitor : AppCompatActivity() {
         actionBar!!.title = "Farm Security"
 
         //local variable declaration
-
-
-        //val currentDate: String = "PI_06_"+SimpleDateFormat("yyyyMMdd", Locale.getDefault()).format(Date())
-        //val currentHour : String = SimpleDateFormat("HH",Locale.getDefault()).format(Date())
-        //val currentMinuteSecond: String = SimpleDateFormat("mmss", Locale.getDefault()).format(Date())
-
-        val currentDateTime = LocalDateTime.now()
-        val dateFormat: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyyMMdd")
-        val hourFormat: DateTimeFormatter = DateTimeFormatter.ofPattern("HH")
-        val minFormat: DateTimeFormatter = DateTimeFormatter.ofPattern("mm")
-        val minSecFormat: DateTimeFormatter = DateTimeFormatter.ofPattern("mmss")
-
-        val dateText = currentDateTime.format(dateFormat)
-        val hourText = currentDateTime.format(hourFormat)
-        val minText = currentDateTime.format(minFormat)
-        val minSecText = currentDateTime.format(minSecFormat).substring(0..2)+0
-        val findDate = "PI_06_"+dateText
-        mDatabase = Firebase.database.reference
-
         val closeAlarm = findViewById<Button>(R.id.btnAlarm)
         //close the alarm
-
         fun readData(){
+            val currentDateTime = LocalDateTime.now()
+            val dateFormat: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyyMMdd")
+            val hourFormat: DateTimeFormatter = DateTimeFormatter.ofPattern("HH")
+            val minFormat: DateTimeFormatter = DateTimeFormatter.ofPattern("mm")
+            val minSecFormat: DateTimeFormatter = DateTimeFormatter.ofPattern("mmss")
+
+            val dateText = currentDateTime.format(dateFormat)
+            val hourText = currentDateTime.format(hourFormat)
+            val minText = currentDateTime.format(minFormat)
+            val minSecText = currentDateTime.format(minSecFormat).substring(0..2)+0
+            val findDate = "PI_06_"+dateText
+            mDatabase = Firebase.database.reference
+            var count : Int = 0
             val ref = mDatabase
                 .child(findDate)//get year Ex. PI_06_20200904
                 .child(hourText)//get hour Ex. 01
                 .child(minSecText)//get hour Ex. 1010
             ref.addValueEventListener(object : ValueEventListener{
                 override fun onCancelled(error: DatabaseError) {
+
                     }
                 override fun onDataChange(p0: DataSnapshot) {
-                    distance.text = "Sound = "+p0.child("sound").value.toString()
-                    Ultra2.text = "Ultra 2 = " + p0.child("ultra2").value.toString()
-                    ultra.text = "Date = " + dateText+hourText+minSecText
+                    if(p0.exists()){
+                        if(!p0.child("sound").value.toString().isNullOrEmpty()){
+                            distance.setText("Sound = "+p0.child("sound").value.toString())
+                            Ultra2.text = "Ultra 2 = " + p0.child("ultra2").value.toString()
+                            ultra.text = "Date = " + dateText+hourText+minSecText
+                            count == 0
+                            //detectdistance = post["ultra2"].toString()
+                            //buzzer = post["buzzer"].toString()
+                        }
+                    }
 
-                    //detectdistance = post["ultra2"].toString()
-                    //buzzer = post["buzzer"].toString()
                 }
             })
         }
+
         //Button Function
         //Security Status Switch
+        val timer = Timer("schedule", true)
+        var counter: Int = 0
         swtAlertMode.setOnCheckedChangeListener{
                 buttonView, isChecked ->
             if(isChecked) {
                 //Start read data from firebase
-                readData()
+                if(counter == 0){
+                    timer.scheduleAtFixedRate(1000,1000){
+                        readData()}
+                }else{
+                    super.onResume()
+                    //timer.scheduleAtFixedRate(1000,1000){
+                    //    readData()}
+                }
                 //verifyDistance()
-                
             }else{
                 //Stop retrieving data from firebase
-                mDatabase.removeEventListener(statelistener)
+                //timer.cancel()
+                super.onPause()
+                counter == 1
+                //mDatabase.removeEventListener(statelistener)
                 val text = "Alarm Closed Successfully"
                 Toast.makeText(applicationContext, text, Toast.LENGTH_SHORT).show()
-
             }
         }
+        //Close Alarm
+
 
 
     }

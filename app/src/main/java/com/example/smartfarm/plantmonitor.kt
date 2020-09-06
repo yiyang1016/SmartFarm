1739d8cf8a2012a9b41a32c5e84ffaf3d584ff69
yiyang1016 <yiyang908@gmail.com>
2020-09-06 10:57:48 +0800

    temperature

--- app/src/main/java/com/example/smartfarm/plantmonitor.kt
+++ app/src/main/java/com/example/smartfarm/plantmonitor.kt
@@ -1,5 +1,11 @@
 package com.example.smartfarm
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
@@ -29,8 +35,11 @@ import kotlin.concurrent.scheduleAtFixedRate
 import android.view.Window
 import android.view.WindowManager
 import android.widget.ImageView
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat
 import com.squareup.picasso.Picasso
+import org.jetbrains.anko.startActivity
 import kotlin.concurrent.schedule
 
 class plantmonitor : AppCompatActivity() {
@@ -40,6 +49,11 @@ class plantmonitor : AppCompatActivity() {
     private lateinit var  statelistener : ValueEventListener
     private lateinit var mDatabase1: DatabaseReference
     internal lateinit var img_1 : ImageView
+    lateinit var notificationManager: NotificationManager
+    lateinit var notificationChannel : NotificationChannel
+    lateinit var  builder: Notification.Builder
+    val channelId="com.example.smartfarm"
+    val description= "Intruder Founded"
     @RequiresApi(Build.VERSION_CODES.O)
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,14 +68,12 @@ class plantmonitor : AppCompatActivity() {
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
         window.statusBarColor = ContextCompat.getColor(this@plantmonitor, R.color.actionBarColor)
-        //Display Picture
-        //val firstImage= "https://thumbs.dreamstime.com/b/anti-intruder-24177968.jpg"
-        //Picasso.get().load(firstImage).into(img_1)
 
-        //val myImageView = findViewById<ImageView>(R.id.myImageView)
         img_1 = findViewById<View>(R.id.myImageView) as ImageView
-        var findDate: String
-        var fullDate: String
+        var findDate: String = ""
+        var fullDate: String = ""
+        var fullMinSec:String = ""
+        var counter: Int = 0
         fun getImage(){
             val currentDateTime = LocalDateTime.now()
             val yearMonthFormat: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyyMM")
@@ -78,11 +90,8 @@ class plantmonitor : AppCompatActivity() {
             val minSecText = currentDateTime.format(minSecFormat).substring(0..2)+0
 
             findDate = yearMonthText+"-"+dateText
-            fullDate = yearMonthText+dateText+hourText+minSecText
-            //imageDate.text = findDate+", "+fullDate
-            //val url_1= "https://firebasestorage.googleapis.com/v0/b/bait2123-202006-06.appspot.com/o/PI_06_CONTROL%2Fcam_20200904235740.jpg?alt=media"
-            val url_1="https://firebasestorage.googleapis.com/v0/b/bait2123-$findDate.appspot.com/o/PI_06_CONTROL%2Fcam_$fullDate.jpg?alt=media"
-            Picasso.get().load(url_1).into(img_1)
+            fullDate = yearMonthText+dateText+hourText
+            fullMinSec = minSecText
         }
         //getImage()
         //local variable declaration
@@ -91,7 +100,8 @@ class plantmonitor : AppCompatActivity() {
 
         val closeAlarm = findViewById<Button>(R.id.btnAlarm)
         mDatabase = Firebase.database.reference
-
+        val intent= Intent(applicationContext, plantmonitor::class.java)
+        val pendingIntent=PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT)
         fun verifyDistanceandSound(){
             if (swtAlertMode.isChecked == true){
                 var convertDistance : Double = detectdistance.toDouble()
@@ -109,8 +119,7 @@ class plantmonitor : AppCompatActivity() {
                     map3["lcd"] = "1"
                     var map4 = mutableMapOf<String,Any>()
                     map4["lcdtext"] = "Intruder Founded"
-                    var map5 = mutableMapOf<String,Any>()
-                    map5["camera"] = "1"
+
 
                     mDatabase.child("PI_06_CONTROL")
                         .updateChildren(map1)
@@ -120,30 +129,65 @@ class plantmonitor : AppCompatActivity() {
                         .updateChildren(map3)
                     mDatabase.child("PI_06_CONTROL")
                         .updateChildren(map4)
-                    mDatabase.child("PI_06_CONTROL")
-                        .updateChildren(map5)
+
+                    val currentDateTime = LocalDateTime.now()
+                    val yearMonthFormat: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyyMM")
+                    val dateFormat: DateTimeFormatter = DateTimeFormatter.ofPattern("dd")
+                    val hourFormat: DateTimeFormatter = DateTimeFormatter.ofPattern("HH")
+                    val minFormat: DateTimeFormatter = DateTimeFormatter.ofPattern("mm")
+                    val minSecFormat: DateTimeFormatter = DateTimeFormatter.ofPattern("mmss")
+
+                    //Change Date Format
+                    val yearMonthText :String = currentDateTime.format(yearMonthFormat)
+                    val dateText = currentDateTime.format(dateFormat)
+                    val hourText = currentDateTime.format(hourFormat)
+                    val minText = currentDateTime.format(minFormat)
+                    val minSecText = currentDateTime.format(minSecFormat).substring(0..2)+0
+
+
+                        findDate = yearMonthText+"-"+dateText
+                        fullDate = yearMonthText+dateText+hourText+minSecText
+                        imageDate.text = "Last intruder detected on : "+findDate
+
 
                     buzzerStatustxt.text = "Buzzer is On"
+                    //Timer("SettingUp", false).schedule(5000) {
+                    //   val url_1="https://firebasestorage.googleapis.com/v0/b/bait2123-"+findDate+".appspot.com/o/PI_06_CONTROL%2Fcam_"+fullDate+".jpg?alt=media"
+                    //   Picasso.get().load(url_1).into(img_1)
+                    //}
 
+                    //Timer("SettingUp", false).schedule(10000) {
+                    //    var map6 = mutableMapOf<String,Any>()
+                    //    map6["camera"] = "0"
+                    //    mDatabase.child("PI_06_CONTROL")
+                    //        .updateChildren(map6)
 
-                    Timer("SettingUp", false).schedule(1000) {
-                        var map6 = mutableMapOf<String,Any>()
-                        map6["camera"] = "0"
-                        mDatabase.child("PI_06_CONTROL")
-                            .updateChildren(map6)
-                            getImage()
+                    //}
+                    if(counter==0){
+
+                        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE)as NotificationManager
+                        notificationChannel= NotificationChannel(channelId,description, NotificationManager.IMPORTANCE_HIGH)
+                        notificationChannel.lightColor=Color.RED
+                        notificationChannel.enableVibration(true)
+                        notificationManager.createNotificationChannel(notificationChannel)
+                        builder= Notification.Builder(this,channelId)
+                            .setContentTitle("Intruder Founded!!! Please Check Your Farm")
+                            .setSmallIcon(R.mipmap.icon_round)
+                            .setContentIntent(pendingIntent)
+                        notificationManager.notify(0,builder.build())
+                        counter +1
+                        val text = "Alarm Activated"
+                        Toast.makeText(applicationContext, text, Toast.LENGTH_SHORT).show()
                     }
-
-                    val text = "Alarm Activated"
-                    Toast.makeText(applicationContext, text, Toast.LENGTH_SHORT).show()
                 }else{
                     msstxt.text = "Still Safe"
                     msstxt.setTextColor(Color.parseColor("#7CFC00"))
                     msstxt.setTextSize(60F)
                 }
             }else{
+                counter = 0
                 msstxt.text = "Please Open To Secure the Farms"
-               msstxt.setTextColor(Color.parseColor("#808080"))
+                msstxt.setTextColor(Color.parseColor("#808080"))
                 msstxt.setTextSize(20F)
             }
         }
@@ -204,10 +248,12 @@ class plantmonitor : AppCompatActivity() {
             }else{
                 val text = "Alarm Off Successfully"
                 Toast.makeText(applicationContext, text, Toast.LENGTH_SHORT).show()
+                var map1 = mutableMapOf<String,Any>()
+                map1["camera"] = "0"
+                mDatabase.child("PI_06_CONTROL")
+                    .updateChildren(map1)
             }
         }
-
-
         //Close Alarm
         closeAlarm.setOnClickListener{
             var buzzerStatus : String = ""
@@ -242,6 +288,8 @@ class plantmonitor : AppCompatActivity() {
                 map2["lcd"] = "0"
                 var map4 = mutableMapOf<String,Any>()
                 map2["lcdtext"] = ""
+                var map5 = mutableMapOf<String,Any>()
+                map2["camera"] = "0"
                 mDatabase.child("PI_06_CONTROL")
                     .updateChildren(map1)
                 mDatabase.child("PI_06_CONTROL")
@@ -250,12 +298,13 @@ class plantmonitor : AppCompatActivity() {
                     .updateChildren(map3)
                 mDatabase.child("PI_06_CONTROL")
                     .updateChildren(map4)
+                mDatabase.child("PI_06_CONTROL")
+                    .updateChildren(map5)
                 buzzerStatustxt.text = "Buzzer is off"
                 val text = "Alarm Closed Successfully"
                 Toast.makeText(applicationContext, text, Toast.LENGTH_SHORT).show()
+                counter = 0
             }
         }
-
-
     }
 }
